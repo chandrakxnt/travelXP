938ed07a20f8e51134a9b7e1136340dfba3f6429
chandrakxnt <105434221+chandrakxnt@users.noreply.github.com>
2024-02-14 00:19:34 +0530

    responsive-bits

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       <aside className=" relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
         <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-16 mx-auto sm:px-6 lg:px-8  mr-16 ml-16">
           <div className=" max-w-xl  sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto ">
-            <h2 className="text-4xl font-bold sm:text-5xl mr-4 lg:text-black sm:text-white">
+            <h2 className="text-4xl font-bold sm:text-5xl mr-4 x  md:text-white sm:text-black ">
               Download Now
               <span className="hidden sm:block text-4xl ">For easy booking</span>
             </h2>
